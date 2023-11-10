472268ebff3016d3f1507ac5836cdc6ab7ef3ff8
Julia Desmazes <julia.desmazes@gmail.com>
2023-11-09 23:58:59 -0600

    TCP: Continue working on rx receiver logic, wip

--- tcp/tcp_rx.v
+++ tcp/tcp_rx.v
@@ -1,6 +1,8 @@
 /* TCP RX module
  * TCP state will be maintained by entry,
- * this module is only for parsing */
+ * this module is only for parsing 
+ *
+ * Only support DATA_W == 16b */
 module tcp_rx#(
 	parameter IS_10G = 1,
 	parameter DATA_W = 16,
@@ -31,6 +33,22 @@ module tcp_rx#(
 	output [LEN_W-1:0]  len_o,
 	output [DATA_W-1:0] data_o	
 );
+/* max head width in btyes */
+localparam MAX_HEAD_N = 57;
+localparam HEAD_CNT_W = $clog2(MAX_HEAD_W);
+ 
+/* FSM */
+logic start_v;
+logic data_end_v;
+logic head_end_v;
+
+logic fsm_idle_next;
+reg   fsm_idle_q;
+logic fsm_head_next;
+reg   fsm_head_q;
+logic fsm_data_next;
+reg   fsm_data_q;
+
 /* TCP header :
  *  0                   1                   2                   3
  *  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
@@ -53,27 +71,79 @@ module tcp_rx#(
  * :                             Data                              :
  * :                                                               |
  * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
- */
+ *
+ * header cnt */
+logic                  unused_cnt_add;
+logic [HEAD_CNT_W-1:0] head_cnt_add;
+logic [HEAD_CNT_W-1:0] head_cnt_next;
+reg   [HEAD_CNT_W-1:0] head_cnt_q;
 
+assign { unused_cnt_add, head_cnt_add} = head_cnt_q + {{HEAD_CNT_W-LEN_W{1'b0}},len_i};
+assign head_cnt_next = start_i ? {{HEAD_CNT_W-LEN_W{1'b0}},len_i}: head_cnt_add;
+always @(posedge clk) begin
+	if ( valid_i ) begin
+		head_cnt_q <= head_cnt_next;
+	end
+end
+ 
 /* store ports */
-logic [PORT_W-1:0] src_port_next;
+logic              src_port_en;
 reg   [PORT_W-1:0] src_port_q;
-logic [PORT_W-1:0] dst_port_next;
+logic              dst_port_en;
 reg   [PORT_W-1:0] dst_port_q;
 
-/* FSM */
-logic start_v;
-logic data_end_v;
-logic head_end_v;
+assign src_port_en = start_v;
+assign dst_port_en = head_cnt_q == 'd2; 
+always @(posedge clk) begin
+	if ( src_port_en ) begin
+		src_port_q <= data_i;
+	end
+	if ( dst_port_en ) begin
+		dst_port_q <= data_i;
+	end
+end
+/* seq and ack number */
+localparam SEQ_EN_W = SEQ_W/DATA_W;
 
-logic fsm_idle_next;
-reg   fsm_idle_q;
-logic fsm_head_next;
-reg   fsm_head_q;
-logic fsm_data_next;
-reg   fsm_data_q;
+logic [SEQ_EN_W-1:0] seq_en;
+logic [SEQ_EN_W-1:0] ack_en;
+reg   [SEQ_W-1:0]    seq_q;
+reg   [SEQ_W-1:0]    ack_q;
 
+genvar i;
+generate
+for(i=0; i<SEQ_EN_W; i++) begin: gen_seq_en_loop
+	assign seq_en[i] = (4 + (i*8/DATA_W)) == head_cnt_q; 
+	assign ack_en[i] = (8 + (i*8/DATA_W)) == head_cnt_q;
+
+	always @(posedge clk) begin
+		if (seq_en[i]) begin
+			seq_q[i*DATA_W+:DATA_W] <= data_i; 
+		end
+		if (ack_en[i]) begin
+			ack_q[i*DATA_W+:DATA_W] <= data_i; 
+		end
+	end
+end
+endgenerate
+/* data offset */
+localparam DOFF_W = 4;
+logic              doff_v;
+reg   [DOFF_W-1:0] doff_q;
+logic [DOFF_W-1:0] doff_next;
+
+assign doff_v = fsm_head_q & ( head_cnt_q > 'd12); 
+always @(posedge clk) begin
+	if ( ~doff_v ) begin
+		doff_q <= data_i[DOFF_W-1:0];
+	end
+end		
+/* flags */
+
+/* FSM logic */
 assign start_v = valid_i & start_i;
+assign head_end_v = valid_i & doff_v & ( doff_q <= head_cnt_next );
+
 assign fsm_idle_next = fsm_idle_q & ~start_v
 					 | fsm_data_q & end_v;
 assign fsm_head_next = fsm_idle_q & start_v
@@ -92,4 +162,16 @@ always @(posedge clk) begin
 	end
 end
 
+`ifdef FORMAL
+
+always @(posedge clk) begin
+	if ( ~nreset ) begin
+		if ( DATA_W == 16 ) begin
+		/* given valid data, the lenght should contain 2 valid bytes with 
+		 * the exeption of the last packet */
+		sva_len_2 : assert( ~valid_i | valid_i & ( len_i == 2'd2 | fsm_idle_next )));
+		end
+	end
+end
+`endif
 endmodule
